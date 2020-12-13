3dc652a4b2c3c42a696a50043d392bfdd5e7e662
PKief <philipp.kief@gmx.de>
2020-12-13 19:35:31 +0100

    Add placeholders for projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'personal-website';
+
+  ngAfterViewInit() {
+    AOS.init({
+      duration: 1000,
+    });
+  }
 }
