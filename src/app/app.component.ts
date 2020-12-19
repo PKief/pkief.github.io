eca80a0784c196af66e315424589b1d6484d629a
PKief <philipp.kief@gmx.de>
2020-12-19 19:34:16 +0100

    Update projects section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import AOS from 'aos';
+import { projects } from './projects';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'personal-website';
+  projects: any[];
+
+  ngOnInit() {
+    this.projects = projects;
+  }
 
   ngAfterViewInit() {
     AOS.init({
