581546e2d64d9880c34f1c71be706ea8c5fe36e9
PKief <philipp.kief@gmx.de>
2020-12-28 15:08:01 +0100

    Update project list component

---
+++ src/app/project/project-list/project-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from '../project.service';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.scss'],
+})
+export class ProjectListComponent implements OnInit {
+  projects: Observable<Project[]>;
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit() {
+    this.projects = this.projectService.getProjects();
+  }
+
+  goToLink(url: string) {
+    window.open(url, '_blank');
+  }
+}
--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, HttpClientModule],
+  declarations: [ProjectListComponent],
+  imports: [CommonModule, HttpClientModule, MatButtonModule],
+  exports: [ProjectListComponent],
 })
 export class ProjectModule {}
